6e084279497ee223433f97926965f8d8cfbedd0d
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-31 12:52:14 +0300

    Initial Commit

---
+++ EscapingClosuresSTBC/EscapingClosuresSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  EscapingClosuresSTBCApp.swift
+//  EscapingClosuresSTBC
+//
+//  Created by Vahtee Boo on 31.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct EscapingClosuresSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
